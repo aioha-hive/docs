48e49a3ecfaea500e5511612f6e835c2068fcfb9
techcoderx <engtianxi@outlook.com>
2024-06-07 22:11:34 +0800

    prepare github pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'aioha-hive', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
